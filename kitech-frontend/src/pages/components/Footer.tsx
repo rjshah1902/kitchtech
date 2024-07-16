ca020449dd63e197007fd00aa7afac141f4d7d53
rjshah1902 <rjshah1902@gmail.com>
2024-07-16 13:50:11 +0530

    copyright fixed

--- kitech-frontend/src/pages/components/Footer.tsx
+++ kitech-frontend/src/pages/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
             <section className="overflow-hidden bg-gray-100 fixed bottom-0 w-full">
                 <div className="container relative z-10 mx-auto px-4">
                     <div className="text-center py-4">
-                        Copywrite © {new Date().getFullYear()}
+                        Copyright © {new Date().getFullYear()}
                     </div>
                 </div>
             </section>
