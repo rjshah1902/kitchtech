f483c822a8373e42a6c94651998d33a8f3f44e9a
rjshah1902 <rjshah1902@gmail.com>
2024-07-31 23:58:02 +0530

    nested if else condition removed on food terminology

--- backend/api/food-terminology/index.php
+++ backend/api/food-terminology/index.php
@@ -5,137 +5,138 @@ require_once './food-terminology.php';
 
 if ($_GET['name'] === 'list') {
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+        
+        return Response::jsonResponse(false, "Request Method Not Allowed");
+
+    }
+
+    $login = new FoodTerminology();
+
+    $loginResponse = $login->list();
+
+    return $loginResponse;
+
+}   
+
+if ($_GET['name'] == 'details') {
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+        
+        return Response::jsonResponse(false, "Request Method Not Allowed");
+
+    }
+        
+    $id = validate_input($_GET['food_terminology_id'], '/^[0-9]+$/' , 'Terminology Id can only contain Numbers'); 
+
+    if (isset($id)) {
 
         $login = new FoodTerminology();
 
-        $loginResponse = $login->list();
+        $userData = $login->singleData($id);
 
-        return $loginResponse;
-
-    } else {
-        
-        return Response::jsonResponse(false, "Request Method Not Allowed");
-    }
-
-}  else if ($_GET['name'] == 'details') {
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        
-        $id = validate_input($_GET['food_terminology_id'], '/^[0-9]+$/' , 'Terminology Id can only contain Numbers'); 
-
-        if (isset($id)) {
-
-            $login = new FoodTerminology();
-
-            $userData = $login->singleData($id);
-
-            return $userData;
-
-        } else {
-            
-            return Response::jsonResponse(false, "Please provide Valid Food Terminology Id");
-        
-        }
+        return $userData;
 
     } else {
         
-        return Response::jsonResponse(false, "Request Method Not Allowed");
-    }
-
-} else if ($_GET['name'] == 'store') {
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        return Response::jsonResponse(false, "Please provide Valid Food Terminology Id");
     
-        $terminology_name = validate_input($_POST['terminology_name'], '/^[a-zA-Z\s]+$/' , 'Terminology Name can only contain letters and spaces');  
-        
-        $food_type_id = validate_input($_POST['food_type_id'], '/^[0-9]+$/' , 'Food Type can only contain Numbers');  
-        
-        $terminology_number = validate_input($_POST['terminology_number'], '/^[0-9]+$/' , 'Terminology Number can only contain Numbers'); 
-
-        if (isset($terminology_name)  && isset($food_type_id) && isset($terminology_number)) {
-
-            $array = array('terminology_name'=>$terminology_name, 'food_type_id'=>$food_type_id,'terminology_number'=>$terminology_number);
-
-            $login = new FoodTerminology();
-
-            $userData = $login->storeFoodTerminologyData($array);
-
-            return $userData;
-
-        } else {
-            
-            return Response::jsonResponse(false, "Please provide Name, Category, Food Type & Terminilogy Number");
-        
-        }
-
-    } else {
-        
-        return Response::jsonResponse(false, "Request Method Not Allowed");
     }
 
-}  else if ($_GET['name'] == 'update') {
+}  
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_GET['name'] == 'store') {
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        
+        return Response::jsonResponse(false, "Request Method Not Allowed");
     
-        $terminology_name = validate_input($_POST['terminology_name'], '/^[a-zA-Z\s]+$/' , 'Terminology Name can only contain letters and spaces');  
-        
-        $food_type_id = validate_input($_POST['food_type_id'], '/^[0-9]+$/' , 'Food Type can only contain Numbers');  
-        
-        $terminology_number = validate_input($_POST['terminology_number'], '/^[0-9]+$/' , 'Terminology Number can only contain Numbers'); 
-        
-        $id = validate_input($_POST['id'], '/^[0-9]+$/' , 'Terminology Id can only contain Numbers'); 
-
-        if (isset($terminology_name)  && isset($food_type_id) && isset($terminology_number)) {
-
-            $array = array('terminology_name'=>$terminology_name, 'food_type_id'=>$food_type_id,'terminology_number'=>$terminology_number);
-
-            $login = new FoodTerminology();
-
-            $userData = $login->updateFoodTerminologyData($array, $id);
-
-            return $userData;
-
-        } else {
-            
-            return Response::jsonResponse(false, "Please provide Name, Category, Food Type & Terminilogy Number");
-        
-        }
-
-    } else {
-        
-        return Response::jsonResponse(false, "Request Method Not Allowed");
     }
-
-}  else if ($_GET['name'] == 'delete') {
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        
-        $id = validate_input($_POST['id'], '/^[0-9]+$/' , 'Terminology Id can only contain Numbers'); 
-
-        if (isset($id)) {
-
-            $login = new FoodTerminology();
-
-            $userData = $login->deleteFoodTerminologyData($id);
-
-            return $userData;
-
-        } else {
-            
-            return Response::jsonResponse(false, "Please provide Food Terminology Id");
-        
-        }
-
-    } else {
-        
-        return Response::jsonResponse(false, "Request Method Not Allowed");
-    }
-
-}   else {
     
-   return Response::jsonResponse(false, "API Not Found");
+    $terminology_name = validate_input($_POST['terminology_name'], '/^[a-zA-Z\s]+$/' , 'Terminology Name can only contain letters and spaces');  
+    
+    $food_type_id = validate_input($_POST['food_type_id'], '/^[0-9]+$/' , 'Food Type can only contain Numbers');  
+    
+    $terminology_number = validate_input($_POST['terminology_number'], '/^[0-9]+$/' , 'Terminology Number can only contain Numbers'); 
+
+    if (isset($terminology_name)  && isset($food_type_id) && isset($terminology_number)) {
+
+        $array = array('terminology_name'=>$terminology_name, 'food_type_id'=>$food_type_id,'terminology_number'=>$terminology_number);
+
+        $login = new FoodTerminology();
+
+        $userData = $login->storeFoodTerminologyData($array);
+
+        return $userData;
+
+    } else {
+        
+        return Response::jsonResponse(false, "Please provide Name, Category, Food Type & Terminilogy Number");
+    
+    }
+
+}  
+
+
+if ($_GET['name'] == 'update') {
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        
+        return Response::jsonResponse(false, "Request Method Not Allowed");
+    
+    }
+    
+    $terminology_name = validate_input($_POST['terminology_name'], '/^[a-zA-Z\s]+$/' , 'Terminology Name can only contain letters and spaces');  
+    
+    $food_type_id = validate_input($_POST['food_type_id'], '/^[0-9]+$/' , 'Food Type can only contain Numbers');  
+    
+    $terminology_number = validate_input($_POST['terminology_number'], '/^[0-9]+$/' , 'Terminology Number can only contain Numbers'); 
+    
+    $id = validate_input($_POST['id'], '/^[0-9]+$/' , 'Terminology Id can only contain Numbers'); 
+
+    if (isset($terminology_name)  && isset($food_type_id) && isset($terminology_number)) {
+
+        $array = array('terminology_name'=>$terminology_name, 'food_type_id'=>$food_type_id,'terminology_number'=>$terminology_number);
+
+        $login = new FoodTerminology();
+
+        $userData = $login->updateFoodTerminologyData($array, $id);
+
+        return $userData;
+
+    } else {
+        
+        return Response::jsonResponse(false, "Please provide Name, Category, Food Type & Terminilogy Number");
+    
+    }
+
+}   
+
+if ($_GET['name'] == 'delete') {
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        
+        return Response::jsonResponse(false, "Request Method Not Allowed");
+
+    }
+
+    $id = validate_input($_POST['id'], '/^[0-9]+$/' , 'Terminology Id can only contain Numbers'); 
+
+    if (!isset($id)) {
+        
+        return Response::jsonResponse(false, "Please provide Food Terminology Id");
+
+    }
+
+    $login = new FoodTerminology();
+
+    $userData = $login->deleteFoodTerminologyData($id);
+
+    return $userData;
 
 }
+    
+
+return Response::jsonResponse(false, "API Not Found");
 
 ?>
