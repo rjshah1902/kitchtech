2576ec2b5f202f61a4441571bbacc63178e79945
rjshah1902 <rjshah1902@gmail.com>
2024-08-01 20:10:02 +0530

    Create common Select tag in frontend

---
+++ kitech-frontend/src/pages/components/SelectTag.tsx
@@ -0,0 +1,25 @@
+import React, { ChangeEvent } from 'react';
+
+interface SelectTagProps {
+    inputValue: string;
+    handleInputChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+    inputName: string;
+    selectArray: any[];
+}
+
+const SelectTag: React.FC<SelectTagProps> = ({ inputName, selectArray, inputValue, handleInputChange }) => {
+    return (
+        <select
+            className="flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 mt-2"
+            name={inputName}
+            value={inputValue}
+            onChange={handleInputChange}>
+            <option value="" disabled> -- Select -- </option>
+            {selectArray.map((data) => (
+                <option key={data.id} value={data.id}>{data.name}</option>
+            ))}
+        </select>
+    );
+}
+
+export default SelectTag;
