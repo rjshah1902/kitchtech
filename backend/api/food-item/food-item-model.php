04a8153e90a88a4271361a7d6e04b0e24d500551
rjshah1902 <rjshah1902@gmail.com>
2024-08-02 21:58:13 +0530

    change validation messages on home residents, terminology & food item page

--- backend/api/food-item/food-item-model.php
+++ backend/api/food-item/food-item-model.php
@@ -6,11 +6,11 @@ function prepareFoodItemData($postData) {
 
     $foodItem['food_name'] = validate_input($postData['food_name'], '/^[a-zA-Z\s]+$/', 'Food Name can only contain letters and spaces');
     
-    $foodItem['food_category_id'] = validate_input($postData['food_category_id'], '/^[0-9]+$/', 'Food Category can only contain Numbers');
+    $foodItem['food_category_id'] = validate_input($postData['food_category_id'], '/^[0-9]+$/', 'Select Food Category');
     
-    $foodItem['food_type_id'] = validate_input($postData['food_type_id'], '/^[0-9]+$/', 'Food Type can only contain Numbers');
+    $foodItem['food_terminology_id'] = validate_input($postData['food_terminology_id'], '/^[0-9]+$/', 'Select Food Terminology');
     
-    $foodItem['food_terminology_id'] = validate_input($postData['food_terminology_id'], '/^[0-9]+$/', 'Food Terminology can only contain Numbers');
+    $foodItem['food_type_id'] = validate_input($postData['food_type_id'], '/^[0-9]+$/', 'Select  Food Type');
     
     return $foodItem;
 }
--- backend/api/home-residents/home-residents-model.php
+++ backend/api/home-residents/home-residents-model.php
@@ -6,9 +6,9 @@ function prepareHomeResidentsData($postData) {
 
     $homeResidents['name'] = validate_input($postData['name'], '/^[a-zA-Z\s]+$/', 'Home Residents Name can only contain letters and spaces');
     
-    $homeResidents['food_type_id'] = validate_input($postData['food_type_id'], '/^[0-9]+$/', 'Food Type can only contain Numbers');
+    $homeResidents['food_terminology_id'] = validate_input($postData['food_terminology_id'], '/^[0-9]+$/', 'Select Food Terminology');
     
-    $homeResidents['food_terminology_id'] = validate_input($postData['food_terminology_id'], '/^[0-9]+$/', 'Food Terminology can only contain Numbers');
+    $homeResidents['food_type_id'] = validate_input($postData['food_type_id'], '/^[0-9]+$/', 'Select Food Terminology');
     
     return $homeResidents;
 }
